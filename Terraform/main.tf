5f3af48063540659a80f406c76484ac1e276640c
rpopov74 <megevand.alexis@gmail.com>
2024-02-06 20:10:52 +0100

    test terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -5,9 +5,9 @@ provider "aws" {
 
 # Création d'un cluster Apache Spark
 resource "aws_instance" "spark_cluster" {
-  ami           = "ami-0c60233f7f38809a4"  # ID de l'AMI Spark
+  ami           = "ami-0c60233f7f38809a4" # ID de l'AMI Spark
   instance_type = "t2.small"              # Type d'instance Spark
-  count         = 3                        # Nombre d'instances dans le cluster
+  count         = 3                       # Nombre d'instances dans le cluster
 
   tags = {
     Name = "Spark-Node-${count.index + 1}"
@@ -16,8 +16,8 @@ resource "aws_instance" "spark_cluster" {
 
 # Création d'une instance MongoDB
 resource "aws_instance" "mongodb_instance" {
-  ami           = "ami-00f5f1e76dfe1aa88"  # ID de l'AMI MongoDB
-  instance_type = "t2.micro"               # Type d'instance MongoDB
+  ami           = "ami-00f5f1e76dfe1aa88" # ID de l'AMI MongoDB
+  instance_type = "t2.micro"              # Type d'instance MongoDB
 
   tags = {
     Name = "MongoDB-Instance"
