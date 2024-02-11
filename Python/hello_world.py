8cd366c789a9ccb555a46d0df4ae681ebc8ec357
rpopov74 <megevand.alexis@gmail.com>
2024-02-11 18:31:18 +0100

    Python

---
+++ Python/hello_world.py
@@ -0,0 +1,17 @@
+from pyspark.sql import SparkSession
+
+# Initialiser une session Spark
+spark = SparkSession.builder \
+    .appName("Hello World PySpark") \
+    .getOrCreate()
+
+# Créer un DataFrame Spark simple
+data = [("Hello World!",)]
+columns = ["Message"]
+df = spark.createDataFrame(data, columns)
+
+# Afficher le message
+df.show()
+
+# Arrêter la session Spark
+spark.stop()
