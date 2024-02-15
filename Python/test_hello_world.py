b71b78786c1c848b653209c715f92dd3d6e3c9d8
rpopov74 <86408305+rpopov74@users.noreply.github.com>
2024-02-15 16:43:08 +0100

    Update test_hello_world.py

--- Python/test_hello_world.py
+++ Python/test_hello_world.py
@@ -1,6 +1,7 @@
 import pytest
 from pyspark.sql import SparkSession
 
+
 @pytest.fixture(scope="module")
 def spark_session():
     """Fixture pour créer une session Spark pour les tests."""
@@ -11,15 +12,16 @@ def spark_session():
     yield spark
     spark.stop()
 
+
 def test_dataframe_creation(spark_session):
     """Teste la création du DataFrame Spark."""
     data = [("Coucou STUDI!",)]
     columns = ["Message"]
     df = spark_session.createDataFrame(data, columns)
-  
+    
     # Vérifier que les nom de la colonne est correcte
     assert df.columns == ["Message"]
-
+    
     # Vérifier que les données dans le DataFrame sont correctes
     collected_data = df.collect()
     assert collected_data[0]["Message"] == "Coucou STUDI!"
